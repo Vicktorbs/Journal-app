8b70657572f2cb5ee285b2707e40c193c9c45c0d
Victor Bolaños Solís <viktorb.132@gmail.com>
2021-07-18 15:23:03 -0500

    Routes protected

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     BrowserRouter as Router,
-    Route,
     Switch,
     Redirect
 } from "react-router-dom";
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
+import { firebase } from '../firebase/firebase_config'
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
+    
+    const [checking, setChecking] = useState(true);
+    const [isLogedIn, setIsLogedIn] = useState(false)
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user?.uid) {
+                dispatch(login(user.uid, user.displayName));
+                setIsLogedIn(true);
+            } else {
+                setIsLogedIn(false)
+            }
+            setChecking(false)
+        })
+    }, [dispatch, setChecking], setIsLogedIn)
+
+    if (checking) {
+        return (
+            <h1>Wait, checking...</h1>
+        )
+    }
+
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route path="/auth" component={ AuthRouter } />
-                    <Route exact path="/" component={ JournalScreen } />
+                    <PublicRoute path="/auth" component={ AuthRouter } isNotAuthenticated={ isLogedIn } />
+                    <PrivateRoute exact path="/" component={ JournalScreen } isAuthenticated= { isLogedIn } />
+                    {/* <Route path="/auth" component={ AuthRouter } />
+                    <Route exact path="/" component={ JournalScreen } /> */}
                     <Redirect to="/auth/login" />
                 </Switch>
             </div>
-        </Router>
+        </Router> 
     )
 }
